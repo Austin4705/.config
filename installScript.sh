80c56ddf8871d54d38d618172330902a98837bdb
Austin Wu <Austin4705@users.noreply.github.com>
2025-02-28 18:57:04 -0500

    commitMessage

--- installScript.sh
+++ installScript.sh
@@ -1,11 +1,13 @@
-echo "INSTALLING"
+echo "INSTALLING (for linux)"
 
 cd ~/
 sudo apt update
 sudo apt install neovim
 sudo apt install clang
+sudo apt install zsh
 
-sudo mv ~/.config/zsh/.zshrc ~/
+sudo mv ~/.config/zsh/.zshrc ~/.zshrc
+sudo mv ~/.config/zsh/.p10k.zsh ~/.p10k.zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo git clone https://github.com/Austin4705/.config.git
