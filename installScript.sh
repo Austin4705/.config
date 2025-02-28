7d26af1e5f9e0c4a7bf4cfb6891226dc9a55e239
Austin Wu <Austin4705@users.noreply.github.com>
2025-02-28 18:41:02 -0500

    commitMessage

---
+++ installScript.sh
@@ -0,0 +1,19 @@
+echo "INSTALLING"
+
+sudo apt update
+sudo apt instlal neovim
+sudo apt install clang
+cd ~/.config
+cd zsh
+mv .zshrc ~/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo git clone https://github.com/Austin4705/.config.git
+
+#downloading
+
+#Installing zsh
+echo $ZSH_THEME
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k\necho 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+source ~/.zshrc
