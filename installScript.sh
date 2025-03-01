9631e7f91f19ad1149a6c8df2ef248e22c0b4913
Austin Wu <Austin4705@users.noreply.github.com>
2025-02-28 23:04:47 -0500

    commitMessage

--- installScript.sh
+++ installScript.sh
@@ -6,6 +6,7 @@ sudo apt -y install clang
 sudo apt -y install zsh
 sudo apt -y install eza
 sudo apt -y install make
+sudo apt -y install clangd
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
 sudo cp ~/.config/zsh/.zshrc ~/.zshrc
