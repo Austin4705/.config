a0c145d354d5b5458de52ef2b6b4a8adb435d761
Austin Wu <Austin4705@users.noreply.github.com>
2025-02-28 22:46:22 -0500

    commitMessage

--- installScript.sh
+++ installScript.sh
@@ -7,8 +7,6 @@ sudo apt -y install zsh
 sudo apt -y install eza
 sudo apt -y install make
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-sudo mv ~/.config/zsh/.zshrc ~/.zshrc
-sudo mv ~/.config/zsh/.p10k.zsh ~/.p10k.zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
-echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+sudo cp ~/.config/zsh/.zshrc ~/.zshrc
+sudo cp ~/.config/zsh/.p10k.zsh ~/.p10k.zsh
