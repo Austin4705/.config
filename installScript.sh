6fed29f5afe00bb1b3fc76f7a2553453b5678b6b
Austin Wu <Austin4705@users.noreply.github.com>
2025-02-28 21:58:44 -0500

    commitMessage

--- installScript.sh
+++ installScript.sh
@@ -1,11 +1,8 @@
 echo "INSTALLING (for linux)"
 
-cd ~/
 sudo apt -y update
 sudo apt -y install neovim
 sudo apt -y install clang
 sudo apt -y install zsh
 sudo apt -y install eza
-
-sudo git clone https://github.com/Austin4705/.config.git ~/.config
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
