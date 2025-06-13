03be6a41db7e2622aff0c2e194591e67a586262d
Austin Wu <Austin4705@users.noreply.github.com>
2025-06-12 22:36:53 -0700

    .

--- nvim/lua/plugins/typst.lua
+++ nvim/lua/plugins/typst.lua
@@ -8,11 +8,11 @@ return {
       vim.g.typst_pdf_viewer = "null"
     end,
   },
-  {
-    -- This creates the web server for live results as well
-    "chomosuke/typst-preview.nvim",
-    lazy = false, -- or ft = 'typst'
-    version = "1.*",
-    opts = {}, -- lazy.nvim will implicitly calls `setup {}`
-  },
+  -- {
+  --   -- This creates the web server for live results as well
+  --   "chomosuke/typst-preview.nvim",
+  --   lazy = false, -- or ft = 'typst'
+  --   version = "1.*",
+  --   opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+  -- },
 }
